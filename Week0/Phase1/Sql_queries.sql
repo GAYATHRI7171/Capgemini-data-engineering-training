2975c1f51c4ab6deb242a63ad33240c13f076968
GAYATHRI7171 <157563042+GAYATHRI7171@users.noreply.github.com>
2026-03-30 15:31:50 +0530

    Update Sql_queries.sql

--- Week0/Phase1/Sql_queries.sql
+++ Week0/Phase1/Sql_queries.sql
@@ -1,1 +1,3 @@
 
+Learned PySpark methods for data loading, transformation, and analysis such as read(), select(), filter(), withColumn(), groupBy(), agg(), and join().
+Also used functions for handling missing data, window operations, SQL queries, and writing data like na.fill(), row_number(), spark.sql(), and write().
